770583e95d74051d60e843f9bea4eb269ff4e64c
Rizki Ramadhan <rizkiramadhan@Rizkis-MacBook-Pro.local>
2014-10-20 15:40:13 +0700

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Rizki Ramadhan on 10/20/14.
+//  Copyright (c) 2014 RizTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
