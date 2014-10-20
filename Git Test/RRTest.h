6be5c761eee6d6550dd801b0c24b50c969a6c38a
Rizki Ramadhan <rizkiramadhan@Rizkis-MacBook-Pro.local>
2014-10-20 16:38:33 +0700

    added a test class

---
+++ Git Test/RRTest.h
@@ -0,0 +1,13 @@
+//
+//  RRTest.h
+//  Git Test
+//
+//  Created by Rizki Ramadhan on 10/20/14.
+//  Copyright (c) 2014 RizTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RRTest : NSObject
+
+@end
---
+++ Git Test/RRTest.m
@@ -0,0 +1,13 @@
+//
+//  RRTest.m
+//  Git Test
+//
+//  Created by Rizki Ramadhan on 10/20/14.
+//  Copyright (c) 2014 RizTech. All rights reserved.
+//
+
+#import "RRTest.h"
+
+@implementation RRTest
+
+@end
